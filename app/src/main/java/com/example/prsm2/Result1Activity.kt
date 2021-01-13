87d685e771a4896ff8126121edb28cc0dd2308f8
tobushiro <tobushiro@gmail.com>
2021-01-13 18:11:43 +0900

    ピンチズーム実装

--- app/src/main/java/com/example/prsm2/Result1Activity.kt
+++ app/src/main/java/com/example/prsm2/Result1Activity.kt
@@ -2,6 +2,8 @@ package com.example.prsm2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_result1.*
 import org.json.JSONObject
@@ -10,9 +12,16 @@ import org.json.JSONObject
 *通常の結果を表示する
 ***************************************************************************************************/
 class Result1Activity : AppCompatActivity() {
+
+    private lateinit var mScaleGestureDetector: ScaleGestureDetector
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result1)
+
+        //ピンチズーム
+        mScaleGestureDetector = ScaleGestureDetector(this, ScaleListener())
+        
         //JSONの準備
         val assetManager = resources.assets
         val inputStream = assetManager.open("maindata.json")
@@ -30,5 +39,22 @@ class Result1Activity : AppCompatActivity() {
         Toast.makeText(this, "出典: " + contentNow.contentSource.toString(), Toast.LENGTH_LONG).show()
         //アクションバーのタイトルを変更
         supportActionBar?.title = contentNow.contentTitle
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        mScaleGestureDetector.onTouchEvent(event)
+        return true//super.onTouchEvent(event)
+    }
+
+    //ピンチズーム
+    private var mScaleFactor = 1.0f
+    private inner class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener(){
+        override fun onScale(detector: ScaleGestureDetector?): Boolean {
+            mScaleFactor *= mScaleGestureDetector.scaleFactor
+            mScaleFactor = Math.max(1.0f, Math.min(mScaleFactor, 5.0f))
+            result1_ivID.scaleX = mScaleFactor
+            result1_ivID.scaleY = mScaleFactor
+            return true
+        }
     }
 }
